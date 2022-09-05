01dd8efcc41487d2b6671520a6743263aeaadca7
Uziano <lucianoperaltadefazio@gmail.com>
2022-09-05 12:36:15 -0300

    Clase 01 terminada

---
+++ clase_01/recordando.py
@@ -0,0 +1,13 @@
+#Vimos comandos básicos, yo practiqué cosas que ya conocía.
+
+print('Hola papuuuuu')
+
+var = int(input('Ingrese un numero: '))
+a = var
+b = 5
+
+def SumaAB(a, b):
+    suma = a + b
+    return suma
+
+print(f'La suma de {a} + {b} es {SumaAB(a,b)}')
